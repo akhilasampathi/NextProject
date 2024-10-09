0eebe9cca3b9b95b126fa835f599959143852a82
AKHILA SAMPATHI <sampathiakhila@gmail.com>
2024-10-09 11:10:36 +0530

    product page some chnages

--- src/product.jsx
+++ src/product.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Product = () => {
   return (
     <center>
-      This is product cart page
+      This is product cart page 
+      new change in product page
     </center>
   );
 }
